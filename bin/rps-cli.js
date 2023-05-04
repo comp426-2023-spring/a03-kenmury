04d9b551dacc742df2289504cdcbfc2ab42c4739
Kenny Murillo <kenmury@email.unc.edu>
2023-05-03 21:54:09 -0400

    rpsls and .bin files updated

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,1 +1,6 @@
 #!/usr/bin/env node
+
+import {RPS} from "../lib/rpsls.js"
+import minimist from "minimist"
+
+RPS();
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,1 +1,6 @@
 #!/usr/bin/env node
+
+import {RPSLS} from "../lib/rpsls.js"
+import minimist from "minimist"
+
+RPSLS();
