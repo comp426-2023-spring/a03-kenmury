bd2772c63a528aa63efc803b5233f7e54e6fc9ee
Kenny Murillo <kenmury@email.unc.edu>
2023-05-03 23:30:37 -0400

    rpsls cli

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,7 +1,84 @@
+#!/usr/bin/env node
+
 import {RPSLS} from "../lib/rpsls.js"
 import minimist from "minimist"
 
-const args = minimist(process.argv.slice(2));
-const choice = args._[0];
+const argv = minimist(process.argv.slice(2));
+const choice = argv._[0];
 
-console.log(choice)
+if (argv.h || argv.help) {
+    console.log(
+        `
+        Usage: node-rpsls [SHOT]
+        Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+        
+          -h, --help        display this help message and exit
+          -r, --rules       display the rules and exit
+        
+        Examples:
+          node-rpsls        Return JSON with single player RPSLS result.
+                            e.g. {"player":"rock"}
+          node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                            e.g {"player":"rock","opponent":"Spock","result":"lose"}
+        `
+    )
+    process.exit(0);
+}
+
+if (argv.r || argv.rules) {
+    console.log(
+        `
+        - Scissors CUTS Paper
+        - Paper COVERS Rock
+        - Rock SMOOSHES Lizard
+        - Lizard POISONS Spock
+        - Spock SMASHES Scissors
+        - Scissors DECAPITATES Lizard
+        - Lizard EATS Paper
+        - Paper DISPROVES Spock
+        - Spock VAPORIZES Rock
+        - Rock CRUSHES Scissors
+        `
+    )
+    process.exit(0)
+}
+
+try {
+    const result = RPSLS(choice);
+    console.log(JSON.stringify(result, null, 2));
+    process.exit(0);
+
+} catch (error) {
+    if (error instanceof RangeError) {
+        console.log(
+            `
+            Usage: node-rpsls [SHOT]
+            Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+            
+              -h, --help        display this help message and exit
+              -r, --rules       display the rules and exit
+            
+            Examples:
+              node-rpsls        Return JSON with single player RPSLS result.
+                                e.g. {"player":"rock"}
+              node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                                e.g {"player":"rock","opponent":"Spock","result":"lose"}
+            `
+        );
+        console.log(
+            `
+            - Scissors CUTS Paper
+            - Paper COVERS Rock
+            - Rock SMOOSHES Lizard
+            - Lizard POISONS Spock
+            - Spock SMASHES Scissors
+            - Scissors DECAPITATES Lizard
+            - Lizard EATS Paper
+            - Paper DISPROVES Spock
+            - Spock VAPORIZES Rock
+            - Rock CRUSHES Scissors
+            `
+        );
+        process.exit(0);
+    }
+}
