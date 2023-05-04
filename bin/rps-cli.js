67a590b73a41809c9a8973f674788a1f779052c0
Kenny Murillo <kenmury@email.unc.edu>
2023-05-03 22:48:43 -0400

    arg h rps

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -6,6 +6,27 @@ import minimist from "minimist"
 const args = minimist(process.argv.slice(2));
 const choice = args._[0];
 
+if (argv.h || argv.help) {
+    console.log(
+        `
+        Usage: node-rps [SHOT]
+        Play Rock Paper Scissors (RPS)
+
+            -h, --help      display this help message and exit
+            -r, --rules     display the rules and exit
+
+        Examples:
+            node-rps        Return JSON with single player RPS result.
+                            e.g. {"player":"rock"}
+            node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                            e.g {"player":"rock","opponent":"scissors","result":"win"}
+        `
+    )
+    process.exit(0);
+}
+
+
+
 
 
 RPS(choice);
