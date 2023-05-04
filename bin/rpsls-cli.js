a84f6d93cbeb322671eaa11b21257e5bd597e1b1
Kenny Murillo <kenmury@email.unc.edu>
2023-05-03 23:42:39 -0400

    fix

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -63,6 +63,8 @@ try {
                                 e.g. {"player":"rock"}
               node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
                                 e.g {"player":"rock","opponent":"Spock","result":"lose"}
+            Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+
             - Scissors CUTS Paper
             - Paper COVERS Rock
             - Rock SMOOSHES Lizard
